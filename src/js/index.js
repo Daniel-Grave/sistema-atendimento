4ed75db099c49799bef85406f49f8db9a4698bf1
Daniel-Grave <danielgrave.grave@gmail.com>
2023-08-21 22:18:48 -0400

    selecionando elementos do DOM

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,9 @@
 import "./libs"
-let inputNome=document.querySelector(inputNome);
+
+const inputNome=document.querySelector('#inputNome');
+const selectPrioridade=documet.querySelector('#selectPrioridade');
+const btnIncluir=document.querySelector('#btnIncluir');
+const ulLista=document.querySelector('#ulLista');
+const displayNome=document.querySelector('#displayNome');
+const btnChamar=displayNome.querySelector('#btnChamar');
+const listaDeAtendimento=new Array();
